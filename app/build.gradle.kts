f710fd0ec09739e24de2155eba831fb3868a0146
enocnehemiasgarciaalcantara <enocgarcia2005@gmail.com>
2023-09-26 20:54:26 -0600

    initial config

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -52,7 +52,7 @@ android {
 dependencies {
 
     //Core
-    implementation("androidx.core:core-ktx:1.9.0")
+    implementation("androidx.core:core-ktx:1.8.10")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
     implementation("androidx.activity:activity-compose:1.7.2")
     implementation(platform("androidx.compose:compose-bom:2023.03.00"))
@@ -62,7 +62,7 @@ dependencies {
     implementation("androidx.compose.material3:material3")
 
     //Navigation
-    implementation("androidx.navigation:navigation-compose:2.7.3")
+    implementation("androidx.navigation:navigation-compose:2.5.3")
 
     //Test
     testImplementation("junit:junit:4.13.2")
